2c35d3acc8793007897564c17e5667bf0fba2034
Rubinatrix <rubinwork@gmail.com>
2016-07-05 11:08:30 +0300

    Bug fix (incorrect imports)

--- src/main/java/controller/mvc/ImageControllerMVC.java
+++ src/main/java/controller/mvc/ImageControllerMVC.java
@@ -1,26 +1,16 @@
 package controller.mvc;
 
-import DTO.IMGGeoTagDTO;
-import domain.TrackIMG;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import service.interfaces.ImageService;
-import utils.GPSTrackerErrorType;
 import utils.GPSTrackerException;
-import utils.geotagreader.GeoTag;
-import utils.geotagreader.JpegGeoTagReader;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class ImageControllerMVC {
